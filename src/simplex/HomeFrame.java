188ef3d7f5999cb4058457d07d24a500db9ca423
mcsosa121 <mcsosa121@gmail.com>
2017-01-09 19:08:48 -0500

    First commit. Added simple jframe

---
+++ src/simplex/HomeFrame.java
@@ -0,0 +1,16 @@
+package simplex;
+
+import javax.swing.JFrame;
+
+public class HomeFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public HomeFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1024, 768);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+}
---
+++ src/simplex/Simplex.java
@@ -0,0 +1,12 @@
+package simplex;
+
+public class Simplex {
+	public static void main(String[] sa) {
+		openHome();
+	}
+	
+	static void openHome() {
+		HomeFrame homepage = new HomeFrame();
+		homepage.setVisible(true);
+	}
+}
