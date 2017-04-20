a9dac228d85ea3c2df460412422d7cd8cff0b0f0
Mike Sosa <mcsosa121@gmail.com>
2017-04-19 23:03:19 -0400

    Worked more on simplex

---
+++ src/simplex/MatrixMult.java
@@ -0,0 +1,15 @@
+package simplex;
+import java.util.*;
+public class MatrixMult {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static Matrix MatrixVecMult(Matrix m, Matrix x) {
+		return m;
+		
+	}
+
+}
--- src/simplex/Simplex.java
+++ src/simplex/Simplex.java
@@ -2,7 +2,17 @@ package simplex;
 
 public class Simplex {
 	public static void main(String[] sa) {
-		openHome();
+		float[][] dog = new float[3][2];
+		for(int i = 0; i<3;i++) {
+			for(int j = 0; j<2; j++) {
+				dog[i][j] = j;
+				//System.out.print(dog[i][j]+" ");
+			}
+			//System.out.println();
+		}
+		Matrix m = new Matrix(3,2,dog);
+		System.out.println(m.toString());
+		//openHome();
 	}
 	
 	static void openHome() {
